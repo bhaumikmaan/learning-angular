5dde865860ad7f8c2aaaea9bdbf2b93b569e4606
bhaumikmaan <bhaumikmaan@gmail.com>
2025-06-30 10:53:51 +0530

    Create Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseComponent } from './course.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CourseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
