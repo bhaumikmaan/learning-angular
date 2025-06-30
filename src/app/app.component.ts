5d31d48169e05c7cfff459f04a3d9e541aded99e
bhaumikmaan <bhaumikmaan@gmail.com>
2025-06-30 10:58:30 +0530

    Component via CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseComponent } from './course.component';
+import { CliComponent } from './cli/cli.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , CourseComponent],
+  imports: [RouterOutlet , CourseComponent , CliComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/cli/cli.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cli',
+  imports: [],
+  templateUrl: './cli.component.html',
+  styleUrl: './cli.component.css'
+})
+export class CliComponent {
+
+}
