321c7b5501b3204130633c50eaa0c35b7000a651
bhaumikmaan <bhaumikmaan@gmail.com>
2025-06-30 15:03:34 +0530

    Two way binding & pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { CourseComponent } from './course.component';
 import { CliComponent } from './cli/cli.component';
 import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , CourseComponent , CliComponent],
+  imports: [RouterOutlet , CourseComponent , CliComponent , FormsModule],
   providers: [CourseService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/course.component.ts
+++ src/app/course.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CourseService } from './course.service';
+import { SummaryPipe } from './summary.pipe';
 
 @Component({
   selector: 'course',
-  imports: [CommonModule],
+  imports: [CommonModule , FormsModule , SummaryPipe],
   template: `
     <h2>{{ getTitle() }}</h2>
     <ul>
@@ -21,6 +24,20 @@ import { CourseService } from './course.service';
         <td [attr.colspan] = "colspan">Cell 3</td>
       </tr>
     </table>
+    <button class="btn btn-primary" [class.active]="isActive"> Button </button>
+
+    <p [style.color]="textColor">This text is styled using style binding</p>
+
+    <div (click)="divOnClick($event)">
+      <button (click)="onClick($event)"> Click Me </button>
+    </div>
+    <input #ipValue (keyup.enter)="onKeyUp(ipValue.value)" />
+    <p> Press Enter. Your input: {{ipValue.value}} </p>
+
+    <input [(ngModel)]="counter" type="number">
+    <button class="btn btn-primary" (click)="incCount()"> Button </button>
+
+    {{ summaryText |  summary:10}}
   `
 })
 export class CourseComponent{
@@ -32,6 +49,34 @@ export class CourseComponent{
   }
   title = "List of Courses";
   courses ;
+  // property binding
   imgUrl = "http://lorempixel.com/400/200";
+  // attribute binding
   colspan = 2;
+  // class binding
+  isActive = true // flip and inspect to see how it is removed
+  // style binding
+  textColor = 'pink'
+  // event binding
+  divOnClick($event: MouseEvent){
+    console.log("Event was bubbled");
+  }
+  onClick($event: MouseEvent){
+    // Toggle to stop event bubbling
+    // $event.stopPropagation();
+    console.log("Button was clicked: " , $event);
+  }
+
+  // template variables
+  onKeyUp(ipValue : string){
+    console.log("Template Variable: " , ipValue);
+  }
+
+  counter = 10;
+  incCount(){
+    this.counter += 1 ;
+    console.log("Incrementing Count to: " , this.counter);
+  }
+
+  summaryText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`;
 }
