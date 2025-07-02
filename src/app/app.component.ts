fd8fc56d444048485cae229cbccc5c066a18d2a8
bhaumikmaan <bhaumikmaan@gmail.com>
2025-07-02 13:09:29 +0530

    Reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { CourseComponent } from './course.component';
 // import { CliComponent } from './cli/cli.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './sign-up/signup-form.component';
 import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , CourseComponent , FormsModule , FavoriteComponent , ContactFormComponent],
+  imports: [RouterOutlet , CourseComponent , ReactiveFormsModule , FormsModule , SignupFormComponent , FavoriteComponent , ContactFormComponent],
   providers: [CourseService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/app/sign-up/signup-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { FormGroup , FormControl , Validators } from '@angular/forms';
+
+@Component({
+  selector: 'signup-form',
+  imports: [ FormsModule , ReactiveFormsModule , CommonModule ],
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent {
+  form = new FormGroup({
+    username: new FormControl(
+      '' , Validators.required
+    ),
+    password: new FormControl(
+      '' , Validators.required
+    )
+  })
+  get username() {
+    return this.form.get('username');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+}
