70ce351b78c84e23ed6a8144f5b2b05622d77c22
bhaumikmaan <bhaumikmaan@gmail.com>
2025-07-02 12:51:20 +0530

    Template form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CourseComponent } from './course.component';
-import { CliComponent } from './cli/cli.component';
+// import { CliComponent } from './cli/cli.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , CourseComponent , CliComponent , FormsModule , FavoriteComponent , ContactFormComponent],
+  imports: [RouterOutlet , CourseComponent , FormsModule , FavoriteComponent , ContactFormComponent],
   providers: [CourseService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
